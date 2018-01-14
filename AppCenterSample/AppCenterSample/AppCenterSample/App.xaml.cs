5600068e9523b2a29fc14ce938dcf6caaa6f4a4d
ytabuchi @ Surface <ytabuchi.xlsoft@gmail.com>
2018-01-14 12:50:37 +0900

    Update with build key

--- AppCenterSample/AppCenterSample/AppCenterSample/App.xaml.cs
+++ AppCenterSample/AppCenterSample/AppCenterSample/App.xaml.cs
@@ -21,9 +21,11 @@ namespace AppCenterSample
         protected override void OnStart()
         {
             if (Xamarin.Forms.Device.RuntimePlatform == Xamarin.Forms.Device.UWP)
-                AppCenter.Start(Secrets.Key, typeof(Analytics));
+                AppCenter.Start("uwp=2b977979-ebbc-40f8-b6d2-cddb98312ac1;",
+                    typeof(Analytics));
             else
-                AppCenter.Start(Secrets.Key, typeof(Analytics), typeof(Crashes));
+                AppCenter.Start("android=a981827b-441f-4ab1-990e-e582d18d60bf;" + "ios=233ad039-4e6d-4baf-8824-3845db2f2907;",
+                    typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
